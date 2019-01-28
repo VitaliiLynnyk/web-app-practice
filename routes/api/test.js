ab6e7f21875862c97902ffe3e1989a622a3b7241
VitaliiLynnyk <linnik2101@gmail.com>
2019-01-28 15:43:22 +0200

    try multiply query

--- routes/api/test.js
+++ routes/api/test.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-let checkAuthentication = require('../../passport/passportLocal').checkAuthentication;
+let checkAuthentication = require("../../passport/passportLocal")
+  .checkAuthentication;
 
 const pool = require("../../db/connection").pool(
   process.env.USER,
@@ -9,13 +10,43 @@ const pool = require("../../db/connection").pool(
   process.env.PGQLPORT
 );
 
-router.get("/test",(req, res, next) => {
-    pool.query(`select * from Person`, (error, results) => {
+router.get("/test", (req, res, next) => {
+  let array = [
+    {
+      firstname: "test",
+      lastname: "test",
+      is_admin: false,
+      email: "test@test.com",
+      hash: "dsaddas"
+    },
+    {
+      firstname: "test",
+      lastname: "test",
+      is_admin: false,
+      email: "test@test.com",
+      hash: "dsaddas"
+    },
+    {
+      firstname: "test",
+      lastname: "test",
+      is_admin: false,
+      email: "test@test.com",
+      hash: "dsaddas"
+    }
+  ];
+  array.forEach(e => {
+    pool.query(
+      `insert into Person (firstname, lastname, is_admin, email, hash) values ($1, $2, $3, $4, $5)`,
+      [e.firstname, e.lastname, e.is_admin, e.email, e.hash],
+      (error, results) => {
         if (error) {
-            return res.status(401).json({message: "Server Error"});
+          return res.status(401).json({ message: "Server Error" });
         }
-        res.status(200).json({message: results.rows});
-    });
+      }
+    );
+  });
+
+  res.status(200).json({ message: "ok" });
 });
 
 module.exports = router;
