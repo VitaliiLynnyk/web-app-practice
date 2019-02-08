e15199c8429827848676e8f9f29629bfc8304383
VitaliiLynnyk <linnik2101@gmail.com>
2019-02-08 12:24:18 +0200

    format returned data in /statistics

--- routes/api/test.js
+++ routes/api/test.js
@@ -11,25 +11,34 @@ const pool = require("../../db/connection").pool(
 );
 
 router.get("/personsTokens", (req, res, next) => {
-    pool.query(`select * from  Person_Token`, (err, data) => {
-        if (err) {
-            return res.status(401).json({ message: "Server Error" });
-        } else if (!data.rows.length) {
-            return res.status(404).json({ message: "Persons Tokens list is Empty" });
-        }
-        res.status(200).send(data.rows);
-    });
+  pool.query(`select * from  Person_Token`, (err, data) => {
+    if (err) {
+      return res.status(401).json({ message: "Server Error" });
+    } else if (!data.rows.length) {
+      return res.status(404).json({ message: "Persons Tokens list is Empty" });
+    }
+    res.status(200).send(data.rows);
+  });
 });
 
 router.get("/statistics", (req, res, next) => {
-    pool.query(`
+  pool.query(
+    `
      select count(*) as allPersons from Person where is_admin=false;
      select count(*) as allSurveys from SURVEY;
      select count(SURVEY.id),STATUS.description from STATUS
      inner join SURVEY on SURVEY.status_id=status.id
-     group by STATUS.description`, (err, data) => {
-        res.status(200).json({ allPersons: data[0].rows,allSurveys: data[1].rows,surveysByStatus: data[2].rows });
-    });
+     group by STATUS.description`,
+    (err, data) => {
+      res
+        .status(200)
+        .json({
+          allPersons: data[0].rows[0].allpersons,
+          allSurveys: data[1].rows[0].allsurveys,
+          surveysByStatus: data[2].rows
+        });
+    }
+  );
 });
 
 module.exports = router;
